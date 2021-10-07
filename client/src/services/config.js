7980df68b1a3bb568be33a0b493695e947c103d9
okamiffs <joshuaramnanan01@gmail.com>
2021-10-07 13:59:25 -0400

    adjusted config

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,9 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
-const baseURL = "http://localhost:3000"
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://p4-pizza-legend-api.herokuapp.com/"
+    : "http://localhost:3000"
 
 const api = axios.create({
   baseURL: baseURL,
-})
+});
 
-export default api
+export default api;
